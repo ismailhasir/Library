ba00c42d1153ea6637522cd5dab48b9c18cde14d
İsmail HASIR <ismailhasir@gmail.com>
2021-11-22 18:35:34 +0300

    design the app from stracth with Bootstrap5

---
+++ app.js
@@ -0,0 +1,16 @@
+/*const newButton = document.getElementById("newBtn");
+
+newButton.addEventListener("click", function () {
+  console.log(Book1);
+});
+
+function Book(title, author, read) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
+}
+
+const Book1 = new Book("Kurt", "Ali", true);
+const Book2 = new Book("Kuzu", "Mehmet", true);
+
+console.log(Book1); */
