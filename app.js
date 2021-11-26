beb6e7280c8f6092931c1c48edd9ae89137a961d
İsmail HASIR <ismailhasir@gmail.com>
2021-11-26 22:15:54 +0300

    finishing js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Book Class:Represents a Book
+// Book Class: Represents a Book
 class Book {
   constructor(title, author, isbn) {
     this.title = title;
@@ -6,36 +6,25 @@ class Book {
     this.isbn = isbn;
   }
 }
-//UI Class: Handle UI Tasks
+
+// UI Class: Handle UI Tasks
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title: "Book One",
-        author: "John Doe",
-        isbn: "3434434",
-      },
-      {
-        title: "Book Two",
-        author: "Jane Doe",
-        isbn: "45545",
-      },
-    ];
-    const books = StoredBooks;
+    const books = Store.getBooks();
 
-    books.forEach((book) => UI.addBooktoList(book));
+    books.forEach((book) => UI.addBookToList(book));
   }
 
-  static addBooktoList(book) {
+  static addBookToList(book) {
     const list = document.querySelector("#book-list");
 
     const row = document.createElement("tr");
 
     row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href='#' class="btn btn-danger btn-sm delete">X</a></td> 
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
 
     list.appendChild(row);
@@ -49,6 +38,14 @@ class UI {
 
   static showAlert(message, className) {
     const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
   }
 
   static clearFields() {
@@ -57,45 +54,81 @@ class UI {
     document.querySelector("#isbn").value = "";
   }
 }
-//Store Class: Handles Storage
+
+// Store Class: Handles Storage
 class Store {
-  static getBooks() {}
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
 
-  static addBook() {}
+    return books;
+  }
 
-  static removeBook()
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
 }
-//Event: Display Books
+
+// Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
-//Event: Add a Book
+// Event: Add a Book
 document.querySelector("#book-form").addEventListener("submit", (e) => {
-  //Prevent actual submit
+  // Prevent actual submit
   e.preventDefault();
 
-  //Get form Values
+  // Get form values
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  //Validate
+  // Validate
   if (title === "" || author === "" || isbn === "") {
-    alert("Please fill inall fields");
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Show success message
+    UI.showAlert("Book Added", "success");
+
+    // Clear fields
+    UI.clearFields();
   }
-
-  //Instatiate book
-  const book = new Book(title, author, isbn);
-
-  //Add Book to UI
-  UI.addBooktoList(book);
-
-  //Clear fields
-  UI.clearFields();
 });
 
-//Event: Remove a Book
+// Event: Remove a Book
 document.querySelector("#book-list").addEventListener("click", (e) => {
+  // Remove book from UI
   UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+  // Show success message
+  UI.showAlert("Book Removed", "success");
 });
-
-
